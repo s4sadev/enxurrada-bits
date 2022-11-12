8cfc93f035c1fb31aaaf02f6e74ba32222986684
Sarah <80551861+s4sadev@users.noreply.github.com>
2022-11-12 16:17:40 -0300

    enxurrada-bits

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Digite o seu nome:")
+let nasc = prompt("Digite o seu ano de nascimento:") 
+let ano = 2022
+let idade = (ano - nasc)
+let total = `Olá, o seu nome é ${nome} e você tem ${idade} anos`
+
+alert(total)
