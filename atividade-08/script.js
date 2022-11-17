5d14ff937d915eb325b65c92a9fa5ab68b49c0ba
Sarah <80551861+s4sadev@users.noreply.github.com>
2022-11-16 21:30:31 -0300

    enxurrada-bits

--- atividade-08/script.js
+++ atividade-08/script.js
@@ -1,1 +1,12 @@
-
+let saldo = prompt('Digite o valor de seu saldo:');
+let compra = prompt('Digite o valor de sua compra:');
+let salatu = saldo - compra
+if(compra<saldo){
+  alert(`Operação bem sucedida, Saldo atual: ${salatu}`)
+}
+else if(compra===saldo){
+  alert(`Operação bem sucedida, Saldo atual: ${salatu}`)
+}
+else {
+  alert(`Operação invalida, saldo insuficiente.`)
+}
