491776fad3564804ce91d90270072b1bca40bee6
Sarah <80551861+s4sadev@users.noreply.github.com>
2022-11-17 21:29:14 -0300

    atividade-08b

--- atividade-08b/script.js
+++ atividade-08b/script.js
@@ -1,1 +1,17 @@
-
+let x = prompt("Digite o primeiro numero:")
+let sinal = prompt("operaçao:")
+let y = prompt("Digite o segundo numero:")
+let num1 = parseInt(x)
+let num2 = parseInt(y)
+if (sinal=== "+"){
+  alert(num1+num2)
+}
+else if (sinal === "-"){
+  alert(num1-num2)
+}
+else if (sinal === "*"){
+  alert(num1*num2)
+}
+else if(sinal=== "/") {
+  alert(num1/num2)
+}
